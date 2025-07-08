16555dc0a9620884710556cdef59a3db9b04fa11
kim-a9 <kimberlysantosalves@gmail.com>
2025-07-08 17:46:35 -0300

    feat: implamentação de teste em GetUser, UpdateUser e DeleteUser

--- src/tests/unit/DeleteUser.test.ts
+++ src/tests/unit/DeleteUser.test.ts
@@ -3,8 +3,22 @@ import {userRepository} from '../../infra/database/repositoryInstance';
 import {DeleteUser} from '../../core/usecases/DeleteUser';
 
 
-describe('DeleteUser', async () => {
+describe('DeleteUser', () => {
+    let user: User
     beforeEach(() => {
         userRepository.users = [];
+        user = new User('1', 'Usuario', 'user01', 'teste@example.com', '123456')
+        userRepository.users.push(user);
+    })
+
+    it('deve apagar um usuario com sucesso', async () => {
+        const deleteUser = new DeleteUser(userRepository);
+        
+        await expect(deleteUser.execute(user.id));
+
+        const deletedUser = await userRepository.findById(user.id);
+
+        expect(deletedUser).toBeNull();
+
     })
 })
--- src/tests/unit/UpdateUser.test.ts
+++ src/tests/unit/UpdateUser.test.ts
@@ -2,13 +2,21 @@ import {User} from '../../core/entities/User';
 import {userRepository} from '../../infra/database/repositoryInstance';
 import {UpdateUser} from '../../core/usecases/UpdateUser';
 
-describe('UpdateUser', async () => {
+describe('UpdateUser', () => {
     let user: User
     beforeEach(() => {
         userRepository.users = [];
         user = new User('1', 'Usuario', 'user01', 'teste@example.com', '123456')
         userRepository.users.push(user);
-    })
+    });
+
+    it('deve atualizar um usuário existente', async () => {
+        const updateUser = new UpdateUser(userRepository);
+
+        const update = await updateUser.execute(user.id, {name: 'Kimberly Alves'});
+
+        expect(update.name).toBe('Kimberly Alves');
+    });
 
 
 
