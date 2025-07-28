d93c95c2725c0dbb4634027f34e66baf5add1827
kim-a9 <kimberlysantosalves@gmail.com>
2025-07-27 22:19:27 -0300

    refactor: testes CreateUser e GetUserByID

--- src/tests/unit/CreateUser.test.ts
+++ src/tests/unit/CreateUser.test.ts
@@ -1,47 +1,66 @@
-// import {CreateUser} from '../../core/usecases/CreateUser';
-// import {User} from '../../core/entities/User';
-// import {userRepository} from '../../infra/database/repositoryInstance';
+import { CreateUser } from '../../core/usecases/CreateUser';
+import { User } from '../../core/entities/User';
+import { InMemoryUserRepository } from '../../infra/database/InMemoryUserRepository';
 
-// describe('CreateUser', () => {
-//     beforeEach(() => {
-//         userRepository.users = [];
-//     })
+describe('CreateUser', () => {    
+    let userRepository: InMemoryUserRepository;
+    let createUser: CreateUser;
 
-//     it('deve criar um novo usuário e guardar no repositorio', async () => {
-//         const createUser = new CreateUser(userRepository);
+   //implementação de testes inMemory 
+    beforeEach(() => {
+    userRepository = new InMemoryUserRepository();
+    createUser = new CreateUser(userRepository);
+    });
+    it('deve criar um novo usuário e guardar no repositorio', async () => {
+        const userData = new User(
+            'Kimberly', 
+            'Kim09',
+            'kimberly@example.com',
+            '123456'
+        );
+        const user = await createUser.execute(userData);
+        const saveUser = await userRepository.findByEmail(userData.email);
 
-//         const user = await createUser.execute({
-//             name: 'Kimberly', 
-//             login: 'Kim09',
-//             email: 'kimberly@example.com',
-//             password: '123456'
-//         });
+        expect(user).toBeInstanceOf(User);
+        expect(user.name).toBe(userData.name);
+        expect(user.login).toBe(userData.login);
+        expect(user.email).toBe(userData.email);
+        expect(saveUser).not.toBeNull();
 
-//         expect(user).toBeInstanceOf(User);
-//         expect(userRepository.users).toHaveLength(1);
-//     });
+    });
 
-//     it('não permite dois usuarios com o mesmo email', async () => {
-//         const createUser = new CreateUser(userRepository);
+    it('não permite dois usuarios com o mesmo email', async () => {
+        const userData = new User(
+            'Kimberly', 
+            'Kim09',
+            'teste@example.com',
+            '123456'
+        );
+        const user = await createUser.execute(userData);
 
-//         const user = await createUser.execute({
-//             name: 'Teste', 
-//             login: 'teste01',
-//             email: 'teste@example.com',
-//             password: '123456'
-//         });
+        await expect(
+            createUser.execute({
+            name: 'Teste 2', 
+            login: 'teste02',
+            email: 'teste@example.com',
+            password: '654321'
+        })
+    ).rejects.toThrow('Usuário já existe com esse e-mail');
+    
+    });
 
-//         await expect(
-//             createUser.execute({
-//             name: 'Teste 2', 
-//             login: 'teste02',
-//             email: 'teste@example.com',
-//             password: '654321'
-//         })
-//     ).rejects.toThrow('Usuário já existe com esse e-mail');
-//     expect(userRepository.users).toHaveLength(1);
-//     });
+    it('deve criptografar a senha do usuário', async () => {
+        const userData = new User(
+            'Kimberly', 
+            'Kim09',
+            'kimberly@example.com',
+            '123456'
+        );
+        const user = await createUser.execute(userData);
 
+        expect(user.password).not.toBe(userData.password); 
+        expect(user.password).toMatch(/^\$2[ayb]\$.{56}$/);
+    });
     
    
-// });
+});
