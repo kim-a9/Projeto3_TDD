fbb951f7c5ee8e935ad64084f568501ff86e5462
kim-a9 <kimberlysantosalves@gmail.com>
2025-07-29 22:38:19 -0300

    conecta ao banco de dados de teste

--- src/tests/integration/CreateUserIntegration.spec.ts
+++ src/tests/integration/CreateUserIntegration.spec.ts
@@ -1,57 +1,55 @@
 import request from 'supertest';
 import app from '../../infra/server/server';
-import { InMemoryUserRepository} from '../../infra/database/InMemoryUserRepository';
-import { User } from '../../core/entities/User';
+import mongoose from 'mongoose';
+import { UserModel } from '../../infra/database/MongooseUserModel';
 
 describe('POST / users', () => {
-    let mockUserRepository: jest.Mocked<InMemoryUserRepository>;
 
-    beforeAll(() => {
-        mockUserRepository = {
-            save: jest.fn(),
-            findByEmail: jest.fn()
-        } as unknown as jest.Mocked<InMemoryUserRepository>;
+    beforeAll(async () => {
+        await mongoose.createConnection(process.env.MONGO_URL_TEST!);
     });
 
-    afterEach(() => {
-        // jest.restoreAllMocks();
-        jest.clearAllMocks();
-    });
-    test('deve criar um novo usuario', async () => {
-    const createdUser = new User(
-        'Dorothy Vaughan',
-        'user01',
-        'teste@example.com',
-        '123456'
-    );
-    mockUserRepository.save.mockResolvedValue(createdUser);
 
-    const response = await request(app).post('/users').send(createdUser);
+    beforeEach(async () => {
+        await UserModel.deleteMany({});
+    });
+
+    afterAll(async () => {
+    await UserModel.deleteMany({});
+    await mongoose.connection.close();
+    }); 
+    it('deve criar um novo usuario', async () => {
+    const response = await request(app).post('/users').send({
+        name:'Dorothy Vaughan',
+        login:'user01',
+        email:'teste@example.com',
+        password: '123456'
+    });
 
     expect(response.status).toBe(200);
 
     });
 
-    it('deve retornar erro caso o email já estiver cadastrado', async () => {
+    // it('deve retornar erro caso o email já estiver cadastrado', async () => {
         
-        await request(app).post('/users').send({
-            name: 'Usuario',
-            login: 'user01',
-            email: 'test@example.com',
-            password: '123456'  
-        });
+    //     await request(app).post('/users').send({
+    //         name: 'Usuario',
+    //         login: 'user01',
+    //         email: 'test@example.com',
+    //         password: '123456'  
+    //     });
         
-        const res = await request(app).post('/users').send({
-            name: 'Kim',
-            login: 'kim09',
-            email: 'test@example.com',
-            password: '654321'
-        });
-        jest.spyOn(mockUserRepository, 'findByEmail').mockRejectedValue('Usuário já existe com esse e-mail');
+    //     const res = await request(app).post('/users').send({
+    //         name: 'Kim',
+    //         login: 'kim09',
+    //         email: 'test@example.com',
+    //         password: '654321'
+    //     });
+    //     jest.spyOn(userRepository, 'findByEmail').mockRejectedValue('Usuário já existe com esse e-mail');
 
-        expect(res.status).toBe(400);
-        expect(res.body).toHaveProperty('error', 'Usuário já existe com esse e-mail');
-    });
+    //     expect(res.status).toBe(400);
+    //     expect(res.body).toHaveProperty('error', 'Usuário já existe com esse e-mail');
+    // });
 
 
 });
--- src/tests/integration/GetUserIntegration.spec.ts
+++ src/tests/integration/GetUserIntegration.spec.ts
@@ -1,34 +1,30 @@
-// import request from 'supertest';
-// import app from '../../infra/server/server';
+import request from 'supertest';
+import app from '../../infra/server/server';
+import mongoose from 'mongoose';
+import { UserModel } from '../../infra/database/MongooseUserModel';
 
-// describe('GET /users/id:', () => {
-//     let userID: string;
-//     beforeAll(async () => {
-//         const { body } = await request(app).post(`/users`).send({
-//         name: 'Usuario',
-//         login: 'user01',
-//         email: 'teste@example.com',
-//         password: '123456'
-//         });
-//         userID = body.id
-//     });
-//     it('deve buscar usuario pelo id', async () => {
-//         const res = await request(app).get(`/users/${userID}`)
+describe('GET /users/id:', () => {
 
-//         expect(res.status).toBe(200);
-//         expect(res.body).toHaveProperty('id');
+     beforeAll(async () => {
+            await mongoose.createConnection(process.env.MONGO_URL_TEST!);
+        });
+    
+        beforeEach(async () => {
+            await UserModel.deleteMany({});
+        });
+    
+        afterAll(async () => {
+        await UserModel.deleteMany({});
+        await mongoose.connection.close();
+        }); 
+
+
+    // it('deve buscar usuario pelo id', async () => {
+    //     const res = await request(app).get(`/users/${userID}`)
+
+    //     expect(res.status).toBe(200);
+    //     expect(res.body).toHaveProperty('id');
         
-//     });
+    // });
 
-
-
-
-
-
-
-
-
-
-
-
-// })
+});
